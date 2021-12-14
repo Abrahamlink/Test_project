6259dc2735bec0272f101af3e394fa88a2b40978
Abrahamlink <cheburekin2014@mail.ru>
2021-12-14 21:22:18 +0500

    Optimize system of marking people for each other — checking relatives between 'getter' and 'giver'

---
+++ families.py
@@ -0,0 +1,3 @@
+families = {'f1': ['Anna', 'Vladimir'], 'f2': ['Maria', 'Stanislav'],
+            'f3': ['Khristina', ''], 'f4': ['Daria', 'Anatolii'],
+            'f5': ['Tatiana', 'Ivan'], 'f6': ['Ekaterina', 'Evgeniy']}
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import random
+from families import families
 
 
 def santa():
-    santas = ['Maria', 'Ivan', 'Anna', 'Khristina', 'Dasha', 'Anatolii', 'Vladimir',
+    """
+    Secret Santa function
+    """
+    santas = ['Maria', 'Ivan', 'Anna', 'Khristina', 'Daria', 'Anatolii', 'Vladimir',
               'Stanislav', 'Evgeniy', 'Ekaterina', 'Tatiana']
 
     for_who = santas.copy()
@@ -10,9 +14,16 @@ def santa():
     while True:
         random.shuffle(for_who)
 
+        # Iterate over givers and getters
         for giver, getter in zip(sorted(santas), for_who):
+            # Test on coincidence
             if getter == giver:
                 continue
+
+            # Test on family
+            for family in families:
+                if getter in family and giver in family:
+                    continue
             result[giver] = getter
 
         for g1, g2 in result.items():
